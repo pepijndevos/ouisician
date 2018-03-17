73e7f5112b3928e5d3bbe61dd93a41e7f6294ae7
Pepijn de Vos <pepijndevos@gmail.com>
2018-03-17 18:58:52 +0100

    wire the I2S bus to the FPGA and blink LED

--- fpga/speaker/speaker.vhd
+++ fpga/speaker/speaker.vhd
@@ -3,8 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity speaker is
-  port(CLOCK_50  : in std_logic;
-		 LEDR      : out std_logic_vector(9 downto 0)
+  port(CLOCK_50    : in std_logic;
+		 LEDR        : out std_logic_vector(9 downto 0);
+		 GPIO_DIN    : in std_logic;
+		 GPIO_LRCK   : in std_logic;
+		 GPIO_BCLK   : in std_logic;
+		 GPIO_DOUT   : out std_logic
 	   );
 end speaker;
 
@@ -12,11 +16,11 @@ architecture Behavioral of speaker is
 	signal counter : unsigned(31 downto 0);
 begin
 
-process(CLOCK_50)
+process(GPIO_LRCK)
 begin
-	if rising_edge(CLOCK_50) then
+	if rising_edge(GPIO_LRCK) then
 		counter <= counter+1;
-		LEDR <= std_logic_vector(counter(31 downto 22));
+		LEDR <= std_logic_vector(counter(9 downto 0));
 	end if;
 end process;
 
