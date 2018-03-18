932795945410bced0ecfeab2bccb99a636e40426
Pepijn de Vos <pepijndevos@gmail.com>
2018-03-18 17:32:25 +0100

    add reset to testbench

--- fpga/impulsebench/mean.vhd
+++ fpga/impulsebench/mean.vhd
@@ -4,6 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity filter is
     port (
+      rst    : in std_logic;
       clk    : in std_logic;
       sndclk : in std_logic;
       word   : in signed(15 downto 0);
@@ -13,11 +14,13 @@ end;
 
 architecture mean of filter is
 begin
-  process(sndclk)
+  process(sndclk, rst)
   type buf_type is array (0 to 3) of signed(15 downto 0);
   variable buf : buf_type;
   begin
-    if rising_edge(sndclk) then
+    if rst = '0' then
+      buf := (others => x"0000");
+    elsif rising_edge(sndclk) then
       buf := word & buf(0 to 2);
       resp <= buf(0)/4 + buf(1)/2 + buf(2)/8 + buf(3)/8;
     end if;
