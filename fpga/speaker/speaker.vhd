33a05b8077021f049514afe798aeb70f01d5b652
Pepijn de Vos <pepijndevos@gmail.com>
2018-03-22 21:03:31 +0100

    Funky FPGA saw waves

--- fpga/speaker/speaker.vhd
+++ fpga/speaker/speaker.vhd
@@ -5,6 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity speaker is
   port(CLOCK_50    : in std_logic;
 		 LEDR        : out std_logic_vector(9 downto 0);
+		 KEY			 : in std_logic_vector(3 downto 0);
 		 GPIO_DIN    : in std_logic;
 		 GPIO_LRCK   : in std_logic;
 		 GPIO_BCLK   : in std_logic;
@@ -15,7 +16,7 @@ end speaker;
 architecture Behavioral of speaker is
 	signal counter : signed(31 downto 0);
 	
-  signal win : signed(63 downto 0);
+  signal win1 : signed(15 downto 0);
   signal wout : signed(63 downto 0);
 begin
 
@@ -25,7 +26,17 @@ begin
 		counter <= counter+1;
 		LEDR <= std_logic_vector(wout(15 downto 6));
 		
-		--win <= wout; -- ECHO
+		if KEY(0) = '0' then
+		  win1 <= counter(5 downto 0) & "0000000000";
+		elsif KEY(1) = '0' then
+		  win1 <= counter(6 downto 0) & "000000000";
+		elsif KEY(2) = '0' then
+		  win1 <= counter(7 downto 0) & "00000000";
+		elsif KEY(3) = '0' then
+		  win1 <= counter(8 downto 0) & "0000000";
+	   else
+		  win1 <= x"0000";
+		end if;
 	end if;
 end process;
 
@@ -35,7 +46,7 @@ end process;
       rlclk =>GPIO_LRCK,
       din => GPIO_DIN,
       dout => GPIO_DOUT,
-      win1 => counter(15 downto 0),
+      win1 => win1,
       win2 => x"00ff",
       win3 => x"5555",
       win4 => x"5555",
