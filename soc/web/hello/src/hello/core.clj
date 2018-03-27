8330d83c71809f9fc22e9536f341fd0cb06f4fd9
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2018-03-27 09:50:34 +0200

    src/

--- soc/web/hello/src/hello/core.clj
+++ soc/web/hello/src/hello/core.clj
@@ -1,6 +1,13 @@
 (ns hello.core)
 
+(response "hello world")
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
+
+; (defn what-is-my-ip [request])
+;   {:status 200
+;     :headers {"Content-Type" "text/plain"}
+;     :body (:remote-addr request)
+;   })
