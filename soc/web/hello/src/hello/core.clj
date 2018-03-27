c4bab321bb3e857342907d3660cabf5df4da2a46
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2018-03-27 10:44:22 +0200

    src/hello/core.clj

--- soc/web/hello/src/hello/core.clj
+++ soc/web/hello/src/hello/core.clj
@@ -1,23 +1,9 @@
-; (ns hello.core)
-
-; (ns ring.example.hello-world-2
- (ns hello 
-  (:use ring.util.response
-        ring.adapter.jetty))
+(ns ring.example.hello-world
+  (:use ring.adapter.jetty))
 
 (defn handler [request]
-  (-> (response "Hello World")
-      (content-type "text/plain")))
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
 
 (run-jetty handler {:port 8080})
-; (response "hello world")
-; (defn handler [request]
-;   {:status 200
-;    :headers {"Content-Type" "text/html"}
-;    :body "Hello World"})
-
-; (defn what-is-my-ip [request])
-;   {:status 200
-;     :headers {"Content-Type" "text/plain"}
-;     :body (:remote-addr request)
-;   })
