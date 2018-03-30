0242a95fc45d8764492cfbc03a46713c45ac1c83
Pepijn de Vos <pepijndevos@gmail.com>
2018-03-30 14:26:13 +0200

    web app that works

--- soc/synth/project.clj
+++ soc/synth/project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [overtone "0.10.3"]]
+                 [overtone "0.10.3"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [javax.servlet/servlet-api "2.5"]]
   :main ^:skip-aot synth.core
   :target-path "target/%s"
   :repl-options { :timeout 120000 }
