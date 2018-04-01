080a246a898673820202aea843e88207fa14b5e9
glhr <grenblot@gmail.com>
2018-04-01 14:18:43 +0200

    more presentable code + sending dummy packet

--- soc/spi/blob/src/blob/core.clj
+++ soc/spi/blob/src/blob/core.clj
@@ -3,28 +3,44 @@
   			[com.pi4j.io.spi SpiChannel SpiDevice SpiFactory]
   			[com.pi4j.util Console]))
 
-(defn printSPIdefaults[]
-	(println "Default SPI settings:")
-	(println (SpiChannel/CS0))
-	(println (SpiDevice/DEFAULT_SPI_SPEED))
-	(println (SpiDevice/DEFAULT_SPI_MODE))
+(defn printSPIsettings
+	([]
+		(println "Using default SPI settings:")
+		(println "- Channel" (SpiChannel/CS0))
+		(println "- Speed" (SpiDevice/DEFAULT_SPI_SPEED) "Hz")
+		(println "- Mode" (.getMode SpiDevice/DEFAULT_SPI_MODE)))
+	([chan]
+		(println "Using:")
+		(println "- Channel" chan)
+		(println "- Speed" (SpiDevice/DEFAULT_SPI_SPEED) "Hz")
+		(println "- Mode" (.getMode SpiDevice/DEFAULT_SPI_MODE)))
 )
 
-(let [console (Console.)]
-	(.title console (into-array String ["boo" "boo"]))
-	(.promptForExit console)
-	(printSPIdefaults)
-	(def spi (SpiFactory/getInstance (SpiChannel/CS0)))
-	(.write spi (byte-array [(byte 0x01) (byte 0x02)]))
-	(while (.isRunning console)
-		(Thread/sleep 1000)
-	)
-	(.emptyLine console)
+(defn setSPIsettings
+	([]
+		(printSPIsettings)
+		(def spi (SpiFactory/getInstance (SpiChannel/CS0)))
+		)
+	([chan]
+		(printSPIsettings chan)
+		(def spi (SpiFactory/getInstance (SpiChannel/getByNumber chan))
+		))
 )
+
+(def packet (byte-array [(byte 0x01) (byte 0x02) (byte 0x03)]))
+
+(setSPIsettings 0)
+
+(def console (Console.))
+(while (.isRunning console)
+	(println "TX" (seq packet))
+	(def rx (.write spi packet))
+	(println "RX" (seq rx))
+	(Thread/sleep 250)
+)
+(.emptyLine console)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-
 )
