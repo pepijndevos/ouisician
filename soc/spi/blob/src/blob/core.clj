ba3c279558f1d1dd607e9822e315e043d1bdf337
glhr <grenblot@gmail.com>
2018-04-02 11:17:08 +0200

    I may or may not be procrastinating philosophy...

--- soc/spi/blob/src/blob/core.clj
+++ soc/spi/blob/src/blob/core.clj
@@ -1,43 +1,35 @@
 (ns blob.core
   (:import 	[java.io IOException]
-  			[com.pi4j.io.spi SpiChannel SpiDevice SpiFactory]
+  			[com.pi4j.io.spi SpiChannel SpiDevice SpiFactory SpiMode]
   			[com.pi4j.util Console]))
 
-(defn printSPIsettings
-	([]
-		(println "Using default SPI settings:")
-		(println "- Channel" (SpiChannel/CS0))
-		(println "- Speed" (SpiDevice/DEFAULT_SPI_SPEED) "Hz")
-		(println "- Mode" (.getMode SpiDevice/DEFAULT_SPI_MODE)))
-	([chan]
+(defn printSPIsettings[& {:keys [chan speed mode]
+							:or {chan (.getChannel SpiChannel/CS0)
+								speed (SpiDevice/DEFAULT_SPI_SPEED)
+								mode (.getMode SpiDevice/DEFAULT_SPI_MODE)}}]
 		(println "Using:")
 		(println "- Channel" chan)
-		(println "- Speed" (SpiDevice/DEFAULT_SPI_SPEED) "Hz")
-		(println "- Mode" (.getMode SpiDevice/DEFAULT_SPI_MODE)))
-)
+		(println "- Speed" speed)
+		(println "- Mode" mode))
 
-(defn setSPIsettings
-	([]
-		(printSPIsettings)
-		(def spi (SpiFactory/getInstance (SpiChannel/CS0)))
-		)
-	([chan]
-		(printSPIsettings chan)
-		(def spi (SpiFactory/getInstance (SpiChannel/getByNumber chan))
-		))
-)
+(defn setSPIsettings[& {:keys [chan speed mode]
+							:or {chan (.getChannel SpiChannel/CS0)
+								speed (SpiDevice/DEFAULT_SPI_SPEED)
+								mode (.getMode SpiDevice/DEFAULT_SPI_MODE)}}]
+		
+		(def spi (SpiFactory/getInstance (SpiChannel/getByNumber chan) speed (SpiMode/getByNumber mode)))
+		(printSPIsettings :chan chan :speed speed :mode mode))
 
 (def packet (byte-array [(byte 0x01) (byte 0x02) (byte 0x03)]))
 
-(setSPIsettings 0)
+(setSPIsettings)
 
 (def console (Console.))
 (while (.isRunning console)
 	(println "TX" (seq packet))
 	(def rx (.write spi packet))
 	(println "RX" (seq rx))
-	(Thread/sleep 250)
-)
+	(Thread/sleep 250))
 (.emptyLine console)
 
 (defn -main
