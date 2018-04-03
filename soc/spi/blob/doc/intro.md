ca87d0d377ec223b25c1b3efda7a4308f5aad2f2
Unknown <grenblot@gmail.com>
2018-04-03 21:48:53 +0200

    no

--- soc/spi/blob/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to blob
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- soc/spi/plop/resources/public/main.js
+++
@@ -1,7 +0,0 @@
-
-var sock = new WebSocket("ws://" + location.host + "/ws");
-
-$('input.slider').change(function() {
-  sock.send(JSON.stringify({id: Number(this.dataset.id), val: Number(this.value), chan: Number(this.dataset.channel)}));
-  console.log(this.value);
-});
