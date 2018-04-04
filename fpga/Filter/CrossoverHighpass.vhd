3f7dd6d0434769d3a84b2b693ea4a3aeb6b334af
prabowobagas <prabowobagas@gmail.com>
2018-04-04 20:08:52 +0200

    Working IIR cascaded filter

--- fpga/Filter/CrossoverHighpass.vhd
+++ fpga/Filter/CrossoverHighpass.vhd
@@ -2,7 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use ieee.NUMERIC_STD.ALL;  
 
-entity CrossoverLowpass is
+entity CrossoverHighpass is
     port (
         main_CLK       : in std_logic;
         Reset          : in std_logic;
@@ -10,41 +10,40 @@ entity CrossoverLowpass is
         data_in         : in signed (15 downto 0);               
         data_out        : out signed (15 downto 0)   -- Output
     );
-end entity CrossoverLowpass;
+end entity CrossoverHighpass;
 
-architecture behaviour of CrossoverLowpass is
+architecture behaviour of CrossoverHighpass is
 
 --Component initiate
-component IIRDF1 is
-    generic (
-        INPUT_WIDTH : integer := 32;
-        QFORMAT   : integer := 30;  
-    B0 : real := 0.00009506; 
-    B1 : real := 0.00019012;
-    B2 : real := 0.00009506;
-    A1 : real := -1.97223373;
-    A2 : real := 0.972613969;
-    A0 : real := 1.0
-    );
-        
-    port (
-        iCLK            : in std_logic;
-        iRESET_N        : in std_logic;
-        new_val         : in std_logic;                         -- indicates a new input value
-        IIR_in          : in signed (15 downto 0);   -- singed is expected             
-        IIR_out         : out signed (15 downto 0)   -- Output
-    );
+component IIRDF1Highpass is
+generic (
+    W_in : integer := 16;
+	 W_coef : integer := 30;   
+    B0 : integer := 4117373; 
+    B1 : integer := -8234746 ;
+    B2 : integer := 4117373;
+    A0 : integer := 4194304;
+    A1 : integer := -8233335 ;
+    A2 : integer := 4041854
+);
+port (
+	iCLK            : in std_logic;
+	iRESET_N        : in std_logic;
+	new_val         : in std_logic;       -- indicates a new input value, input from data_over
+	IIR_in          : in signed (15 downto 0);   -- singed is expected             
+	IIR_out         : out signed (15 downto 0)   -- Output
+);
 end component;
 
 --Connection signals
 signal signal_in : signed(15 downto 0) := (others=>'0');
 signal IIR_connect : signed(15 downto 0) := (others=>'0');
-signal signal_out : signed(15 downto 0) := (others=>'0');
+signal signal_out : signed(15 downto 0) := x"0000";
 
 begin
 signal_in <= data_in;
 data_out <=  signal_out;
-IIR1_LPF : IIRDF1
+IIR1_LPF : IIRDF1Highpass
 port map (
 	iCLK        => main_CLK,
 	iRESET_N    => Reset,
@@ -53,12 +52,12 @@ port map (
 	IIR_out     => IIR_connect
 );
 
-IIR2_LPF : IIRDF1
+IIR2_LPF : IIRDF1Highpass
 port map (
 	iCLK        => main_CLK,
 	iRESET_N    => Reset,
 	new_val    => new_val  ,
-	IIR_in     => signal_in,
+	IIR_in     => IIR_connect,
 	IIR_out     => signal_out
 );
 
