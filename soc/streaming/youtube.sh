0d6e6fc3d20b0a263973c2ea3184f3fcb24d09fc
Ubuntu User <grenblot@gmail.com>
2018-04-05 19:56:39 +0200

    script for youtube streaming

--- soc/streaming/youtube.sh
+++ soc/streaming/youtube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-STREAM_KEY=live_205865829_ICeYXGi2sw5IntQ2uQZavJ9MqWjxDo
-URL=rtmp://live.twitch.tv/app/$STREAM_KEY
+STREAM_KEY=17mp-vm1y-kpzd-2998
+URL=rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY
 
 ffmpeg -f concat -i list.txt \
 -f alsa -i pulse -map 0:v -map 1:a \
