f381ef7336db1f80ff3c69e86d784402c9d97b97
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2018-04-09 14:20:50 +0200

    dac.py

--- soc/spi/raspberrypi/dac.py
+++ soc/spi/raspberrypi/dac.py
@@ -1,21 +1,18 @@
 import spidev
+import RPi.GPIO as GPIO
+
 spi=spidev.SpiDev()
 #open spi bus 0 and device 0
 spi.open(0,0)
-spi.max_speed_hz=976000
 
-refFreq=50*10**6
-#2^28=268435456
-Two28=268435456
-phase=0
 
-def send_data(fre):
-    freWord=fre*Two28/refFreq
+def writeFrequency(freq):
+    freWord=int (freq*2**28/refFreq)
     MSB=int((freWord & 0xfffc000) >> 14)
     LSB=int(freWord & 0x3fff)
     LSB|=0x4000
     MSB|=0x4000
-    spi.xfer([LSB,MSB])
+    spi.xfer([LSB,MSB,0x2000])
 
 while True:
     send_data(1000)
