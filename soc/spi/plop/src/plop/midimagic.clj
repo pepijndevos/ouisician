4a4caac2aa1c64193a8547a55c8f5c94c832e918
Unknown <grenblot@gmail.com>
2018-04-10 11:41:49 +0200

    midimagic at startup #dac.py

---
+++ soc/spi/plop/src/plop/midimagic.clj
@@ -0,0 +1,13 @@
+(ns plop.midimagic
+  (:import 	[java.io IOException]
+			[java.lang Process]
+  			[java.lang ProcessBuilder]))
+			
+(defn startmidi []
+	(def fluidsynth-cmd "fluidsynth -a pulseaudio -m alsa_seq /home/pi/ChoriumRevA/ChoriumRevA.SF2")
+	(def pb-fluidsynth (ProcessBuilder. (list "/bin/bash" "-c" fluidsynth-cmd)))
+	(def process-fluidsynth (.start pb-fluidsynth))
+	(Thread/sleep 1000)
+	(def aconnect-cmd "aconnect 24:0 128:0")
+	(def pb-aconnect (ProcessBuilder. (list "/bin/bash" "-c" aconnect-cmd)))
+	(def process-aconnect (.start pb-aconnect)))
