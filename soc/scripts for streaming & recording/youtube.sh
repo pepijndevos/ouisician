aae91d71eac691701c8ab9525b41dbb3b2c7e14c
Unknown <grenblot@gmail.com>
2018-04-12 09:29:18 +0200

    i wanna die dac dot pie

--- soc/scripts for streaming & recording/youtube.sh
+++ soc/scripts for streaming & recording/youtube.sh
@@ -4,6 +4,6 @@ STREAM_KEY=17mp-vm1y-kpzd-2998
 URL=rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY
 
 ffmpeg -f concat -i list.txt \
--f alsa -i pulse -map 0:v -map 1:a \
+-f alsa -i default -map 0:v -map 1:a \
 -c:v copy -c:a aac -ar 44100 -b:a 96k \
 -f flv $URL
--- streaming/processbuilder/twitch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STREAM_KEY=live_205865829_ICeYXGi2sw5IntQ2uQZavJ9MqWjxDo
-URL=rtmp://live.twitch.tv/app/$STREAM_KEY
-
-ffmpeg -f concat -i list.txt \
--f alsa -i pulse -map 0:v -map 1:a \
--c:v copy -c:a aac -ar 44100 -b:a 96k \
--f flv $URL
