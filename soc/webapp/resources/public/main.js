f8441ad689a95df135d024bf97ac285dead17e69
glhr <grenblot@gmail.com>
2018-04-13 12:58:10 +0000

    added oua oua and gauchissement...only people with big IQ will understand

--- soc/webapp/resources/public/main.js
+++ soc/webapp/resources/public/main.js
@@ -27,17 +27,36 @@ sock.onmessage = function(e) {
     $('#' + msg.id).val(msg.display);
   }
 
-  
+  if (msg.id == "tremolo-2-check") {
+    if(msg.display == 1) {
+      $("#tremolo-2-check").prop('checked', true);
+      ;$("#tremolo-2-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#tremolo-2-check").prop('checked', false);
+      ;$("#tremolo-2-slide").attr('disabled', true);
+    }
+  }
+   if (msg.id == "tremolo-1-check") {
+    if(msg.display == 1) {
+      $("#tremolo-1-check").prop('checked', true);
+      ; $("#tremolo-1-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#tremolo-1-check").prop('checked', false);
+      ;$("#tremolo-1-slide").attr('disabled', true);
+    }
+  }
 
 
   if (msg.id == "recording") {
     
     if(msg.numid == 1) { //starting recording
-      $('#recording').prop('checked', true);
+      $("#recording").prop('checked', true);
       $( "#record-log" ).append( '<div class="card-text row"><div class="col-12"><small>Recording to ' + msg.display + "</small></div></div>" );
     }
     else if(msg.numid == 0) { //done recording
-      $('#recording').prop('checked', false);
+      $("#recording").prop('checked', false);
       $( "#record-log" ).append( '<div class="card-text row"><div class="col-12"><small>Done recording to ' + msg.display + "</small></div></div>" );
       addAudioFile(msg.display);
     }
@@ -49,12 +68,12 @@ sock.onmessage = function(e) {
 
   if (msg.id == "streaming") {
     if(msg.numid == 1) {
-      $('#streaming').prop('checked', true);
+      $("#streaming").prop('checked', true);
       $("#streamplatform").attr('disabled',true);
       $("#streamkey").attr('disabled',true);
     }
     else if(msg.numid == 0) {
-      $('#streaming').prop('checked', false);
+      $("#streaming").prop('checked', false);
       $("#streamplatform").attr('disabled',false);
       $("#streamkey").attr('disabled',false);
     }
@@ -74,7 +93,7 @@ $('input.form-check-input').change(function() {
   if ((this.id == "streaming") || (this.id == "recording")) {
 	sock.send(JSON.stringify({numid: Number(checked), id: this.id, val: $('#streamkey').val(), chan: Number(0), platform: Number($( "#streamplatform option:selected" ).val())}));
   }
-  if ((this.id == "tremolo-1") || (this.id == "tremolo-2")) {
+  if ((this.id == "tremolo-1-check") || (this.id == "tremolo-2-check")) {
 	sock.send(JSON.stringify({numid: Number(this.dataset.id), id: this.id, val: Number(checked), chan: Number(this.dataset.channel), platform: 0}));
   }
   console.log(this.value);
