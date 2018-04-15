8a2b50050efbaceb47d0c8c63c58f952c307403a
Pepijn de Vos <pepijndevos@gmail.com>
2018-04-15 20:46:57 +0200

    reorder stuff

--- fpga/speaker/speaker.vhd
+++ fpga/speaker/speaker.vhd
@@ -139,6 +139,38 @@ begin
 	end if;
 end process;
 
+  normalization_inst : entity work.normalization
+	port map (
+		clk50mhz => clk,
+		pot_clk => pot_clk,
+		reset => rst,
+		KEY => KEY,
+		ic => potic,
+		chan => chan_temp, 
+		filterid => filterid_temp,
+		fil_data => filterdata_temp
+		);
+		
+  adc_inst1: entity work.adc(behavioral)
+    generic map (
+      use_fir => false
+    )
+    port map (rst => rst,
+      clk => clk,
+		sndclk => sndclk2,
+      data => GPIO_ADCDAT1,
+      word => myadc1);
+		
+  adc_inst2: entity work.adc(behavioral)
+    generic map (
+      use_fir => false
+	 )
+    port map (rst => rst,
+      clk => clk,
+		sndclk => sndclk3,
+      data => GPIO_ADCDAT2,
+      word => myadc2);
+
 --Tremolo_inst : entity work.Tremolo_FX(behaviour)
 --port map(
 --	data_in => mixed,
@@ -151,15 +183,7 @@ end process;
 --	fil_data => filterdata_temp
 --);
 
-  crossover_inst: entity work.Crossover
-	port map (
-      main_CLK => clk,
-      Reset => rst,
-      new_val => sndclk,
-      data_in => mixed,
-      data_outlow => wout1,
-		data_outhigh => wout2
-		);
+
 		
  comb_inst1 : entity work.comb
 	port map (
@@ -258,7 +282,6 @@ end process;
     data => offset
 );
 
-
   mixer_inst: entity work.mixer
     port map (rst => rst,
       clk => sndclk,
@@ -267,82 +290,7 @@ end process;
       word3 => pi1,
       word4 => pi2,
       resp => mixed);
-		
-		
-  i2s_inst: entity work.i2s
-    port map (rst => rst,
-      bclk => bitclk,
-      rlclk => GPIO_LRCK,
-      din => GPIO_DIN,
-      dout => GPIO_DOUT,
-      win1 => mixed,
-      win2 => mixed,
-      wout1 => pi1,
-      wout2 => pi2);
 
-  adc_inst1: entity work.adc(behavioral)
-    generic map (
-      use_fir => false
-    )
-    port map (rst => rst,
-      clk => clk,
-		sndclk => sndclk2,
-      data => GPIO_ADCDAT1,
-      word => myadc1);
-		
-  adc_inst2: entity work.adc(behavioral)
-    generic map (
-      use_fir => false
-	 )
-    port map (rst => rst,
-      clk => clk,
-		sndclk => sndclk3,
-      data => GPIO_ADCDAT2,
-      word => myadc2);
-		
-  normalization_inst : entity work.normalization
-	port map (
-		clk50mhz => clk,
-		pot_clk => pot_clk,
-		reset => rst,
-		KEY => KEY,
-		ic => potic,
-		chan => chan_temp, 
-		filterid => filterid_temp,
-		fil_data => filterdata_temp
-		);  
-  
-	audio_inst : entity work.audio_interface
-		port map (
-			LDATA => std_logic_vector(wout1),
-			RDATA => std_logic_vector(wout2),
-			clk => clk,
-			Reset	=> rst,
-			INIT_FINISH	=> open,
-			adc_full	=> open,
-			AUD_MCLK => AUD_XCK,
-			AUD_ADCLRCK => AUD_ADCLRCK,
-			AUD_ADCDAT => AUD_ADCDAT,
-			AUD_BCLK => AUD_BCLK,
-			data_over => sndclk,
-			AUD_DACDAT => AUD_DACDAT,
-			AUD_DACLRCK => AUD_DACLRCK,
-			I2C_SDAT => FPGA_I2C_SDAT,
-			I2C_SCLK => FPGA_I2C_SCLK,
-			ADCDATA => socadc
-		);
-	
-	pll_inst: pll
-		port map (
-			refclk => CLOCK_50,
-			rst => SW(9),
-			outclk_0 => bitclk,  -- 1.536 MHz
-			outclk_1 => clk, -- 49.152 MHz
-			outclk_2 => halfclk, -- 24.576 MHz
-			locked => rst);
-			
-			
-			
 --	Equalizer : entity work.Equalizermain -- equalizer Port/signal => main port/ignal
 --	port map (
 --        	main_CLK => clk,      
@@ -364,7 +312,58 @@ end process;
 --		filterdataEQ => filterdata_temp
 --	);
 
-SPIhandlerEqualizer : entity work.spi_slave_ui
+  crossover_inst: entity work.Crossover
+	port map (
+      main_CLK => clk,
+      Reset => rst,
+      new_val => sndclk,
+      data_in => mixed,
+      data_outlow => wout1,
+		data_outhigh => wout2
+		);
+
+	audio_inst : entity work.audio_interface
+		port map (
+			LDATA => std_logic_vector(wout1),
+			RDATA => std_logic_vector(wout2),
+			clk => clk,
+			Reset	=> rst,
+			INIT_FINISH	=> open,
+			adc_full	=> open,
+			AUD_MCLK => AUD_XCK,
+			AUD_ADCLRCK => AUD_ADCLRCK,
+			AUD_ADCDAT => AUD_ADCDAT,
+			AUD_BCLK => AUD_BCLK,
+			data_over => sndclk,
+			AUD_DACDAT => AUD_DACDAT,
+			AUD_DACLRCK => AUD_DACLRCK,
+			I2C_SDAT => FPGA_I2C_SDAT,
+			I2C_SCLK => FPGA_I2C_SCLK,
+			ADCDATA => socadc
+		);
+
+  i2s_inst: entity work.i2s
+    port map (rst => rst,
+      bclk => bitclk,
+      rlclk => GPIO_LRCK,
+      din => GPIO_DIN,
+      dout => GPIO_DOUT,
+      win1 => mixed,
+      win2 => mixed,
+      wout1 => pi1,
+      wout2 => pi2);
+
+	pll_inst: pll
+		port map (
+			refclk => CLOCK_50,
+			rst => SW(9),
+			outclk_0 => bitclk,  -- 1.536 MHz
+			outclk_1 => clk, -- 49.152 MHz
+			outclk_2 => halfclk, -- 24.576 MHz
+			locked => rst);
+			
+
+  SPIhandler : entity work.spi_slave_ui
 	port map (
       clk => clk,	
 		reset_n	 => rst,
