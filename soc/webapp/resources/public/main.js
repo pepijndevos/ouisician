e33d3764e32173274b8b76316b08c40f43631a4f
glhr <grenblot@gmail.com>
2018-04-17 14:22:17 +0000

    UI unfuckery (hopefully)

--- soc/webapp/resources/public/main.js
+++ soc/webapp/resources/public/main.js
@@ -36,21 +36,65 @@ sock.onmessage = function(e) {
   if (msg.id == "tremolo-2-check") {
     if(msg.display == 1) {
       $("#tremolo-2-check").prop('checked', true);
-      ;$("#tremolo-2-slide").attr('disabled',false);
+      //$("#tremolo-2-slide").attr('disabled',false);
     }
 	else if(msg.display == 0) {
       $("#tremolo-2-check").prop('checked', false);
-      ;$("#tremolo-2-slide").attr('disabled', true);
+      //$("#tremolo-2-slide").attr('disabled', true);
     }
   }
    if (msg.id == "tremolo-1-check") {
     if(msg.display == 1) {
       $("#tremolo-1-check").prop('checked', true);
-      ; $("#tremolo-1-slide").attr('disabled',false);
+      //$("#tremolo-1-slide").attr('disabled',false);
     }
 	else if(msg.display == 0) {
       $("#tremolo-1-check").prop('checked', false);
-      ;$("#tremolo-1-slide").attr('disabled', true);
+      //$("#tremolo-1-slide").attr('disabled', true);
+    }
+  }
+  
+     if (msg.id == "wawa-1-check") {
+    if(msg.display == 1) {
+      $("#wawa-1-check").prop('checked', true);
+      ; $("#wawa-1-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#wawa-1-check").prop('checked', false);
+      //$("#wawa-1-slide").attr('disabled', true);
+    }
+  }
+  
+       if (msg.id == "wawa-2-check") {
+    if(msg.display == 1) {
+      $("#wawa-2-check").prop('checked', true);
+      // $("#wawa-2-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#wawa-2-check").prop('checked', false);
+      //$("#wawa-2-slide").attr('disabled', true);
+    }
+  }
+  
+   if (msg.id == "distortion-1-check") {
+    if(msg.display == 1) {
+      $("#distortion-1-check").prop('checked', true);
+      //$("#distortion-1-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#distortion-1-check").prop('checked', false);
+      //$("#distortion-1-slide").attr('disabled', true);
+    }
+  }
+  
+     if (msg.id == "distortion-2-check") {
+    if(msg.display == 1) {
+      $("#distortion-2-check").prop('checked', true);
+      //$("#distortion-2-slide").attr('disabled',false);
+    }
+	else if(msg.display == 0) {
+      $("#distortion-2-check").prop('checked', false);
+      //$("#distortion-2-slide").attr('disabled', true);
     }
   }
 
@@ -127,7 +171,7 @@ $('[id^=flangerpreset-ch]').change(function() {
 		$('#offset2-'+this.dataset.channel).val("0");
 		$('#offset3-'+this.dataset.channel).val("0");
 
-		$('#blgain-'+this.dataset.channel).val("0");
+		$('#blgain-'+this.dataset.channel).val("255");
 
 		$('#ffgain1-'+this.dataset.channel).val("0");
 		$('#ffgain2-'+this.dataset.channel).val("0");
@@ -179,7 +223,7 @@ $('[id^=flangerpreset-ch]').change(function() {
 	$('#tw-speed').val("0");
 	$('#tw-width').val("0");
 	
-    $('#offset1-'+this.dataset.channel).val("1000000");
+    $('#offset1-'+this.dataset.channel).val("50000");
     $('#offset2-'+this.dataset.channel).val("1000000");
     $('#offset3-'+this.dataset.channel).val("1000000");
     $('#blgain-'+this.dataset.channel).val("255");
@@ -212,8 +256,8 @@ $('[id^=flangerpreset-ch]').change(function() {
   }
   //VIBRATO
   else if($(this).val() == "6") {
-	$('#tw-speed').val("1000");
-	$('#tw-width').val("2000");
+	$('#tw-speed').val("5000");
+	$('#tw-width').val("1000");
 	$('#blgain-'+this.dataset.channel).val("0");
 	
 	$('#ffgain1-'+this.dataset.channel).val("255");
@@ -247,6 +291,10 @@ $('#reset').click(function() {
 	$('.slider').each(function() {
 		$('#' + this.id).val(this.dataset.default);
 		sock.send(JSON.stringify({numid: Number(this.dataset.id), id: this.id, val: Number(this.value), chan: Number(this.dataset.channel), platform: 0}));
+	});
+	
+	$("[id^=flangerpreset-ch]").each(function() {
+		$('#' + this.id).val("1");
 	});
 	
 
--- soc/webapp/src/plop/midimagic.clj
+++ soc/webapp/src/plop/midimagic.clj
@@ -4,7 +4,7 @@
   			[java.lang ProcessBuilder]))
 			
 (defn startmidi []
-	(def fluidsynth-cmd "fluidsynth -a pulseaudio -m alsa_seq /home/pi/ChoriumRevA/ChoriumRevA.SF2")
+	(def fluidsynth-cmd "fluidsynth -a pulseaudio -m alsa_seq /home/pi/ChoriumRevA/ChoriumRevA.SF2 -g 5")
 	(def pb-fluidsynth (ProcessBuilder. (list "/bin/bash" "-c" fluidsynth-cmd)))
 	(def process-fluidsynth (.start pb-fluidsynth))
 	(Thread/sleep 1000)
